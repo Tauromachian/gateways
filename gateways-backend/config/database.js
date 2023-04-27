2055f8b675603dbc7b6bc8243655ed6ae439eceb
Jose Garcia <jogarcia@tuta.io>
2023-04-27 18:21:01 -0400

    Make basic connection file

---
+++ gateways-backend/config/database.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const DB_URI = "mongodb://localhost:27017/gateway"; // Change this to your database URI
+
+mongoose.connect(DB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
+
+const db = mongoose.connection;
+
+db.on("error", (error) => {
+  console.error("MongoDB connection error:", error);
+});
+
+db.once("open", () => {
+  console.log("MongoDB connected successfully!");
+});
+
+module.exports = db;

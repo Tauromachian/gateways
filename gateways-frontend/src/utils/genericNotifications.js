96b8b3c4ef09dcea29854fc5fd82b4331b5d89b8
Jose Garcia <jogarcia@tuta.io>
2023-04-29 10:51:21 -0400

    Add generic notifications

---
+++ gateways-frontend/src/utils/genericNotifications.js
@@ -0,0 +1,36 @@
+const genericNotifications = {
+  selectRowBeforeUpdate: {
+    message: "Select a row before updating please",
+    color: "info",
+  },
+  selectRowBeforeDelete: {
+    message: "Select a row before deleting please",
+    color: "info",
+  },
+
+  successfulInsert: {
+    message: "You have inserted successfully",
+    color: "success",
+  },
+  successfulDelete: {
+    message: "You have deleted successfully",
+    color: "success",
+  },
+  successfulUpdate: {
+    message: "You have updated successfully",
+    color: "success",
+  },
+
+  errorInsert: {
+    message: "There was a problem inserting",
+    color: "success",
+  },
+  errorDelete: {
+    message: "There was a problem deleting",
+    color: "success",
+  },
+  errorUpdate: {
+    message: "There was a problem updating",
+    color: "success",
+  },
+};

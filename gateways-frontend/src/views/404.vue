963664fdf166b1885b4886d22b8c33ce405e2493
Jose Garcia <jogarcia@tuta.io>
2023-04-30 21:29:55 -0400

    Improve 404 page

--- gateways-frontend/src/views/404.vue
+++ gateways-frontend/src/views/404.vue
@@ -1,15 +1,22 @@
 <template>
-  <div class="error404">
-    Error 404
-  </div>
+  <v-container max-width="700px" class="relative">
+    <img src="@/assets/page-not-found.svg" alt="not-found" />
+    <div class="d-flex flex-column justify-center align-center mt-4 back-home">
+      <p>You are little lost here mate you may want to:</p>
+      <v-btn
+        :to="{ name: 'gateway' }"
+        elevation="0"
+        text
+        class="mt-2"
+        color="primary"
+        prepend-icon="mdi-home"
+      >
+        Go back home
+      </v-btn>
+    </div>
+  </v-container>
 </template>
 
-<script>
-  export default {
-    name: '404'
-  }
-</script>
+<script setup></script>
 
-<style scoped>
-
-</style>
+<style></style>

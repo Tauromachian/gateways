d886c76f4e467e419cf39857f1ad546f329772fa
Jose Garcia <jogarcia@tuta.io>
2023-05-01 14:46:36 -0400

    Replace jest with vitest

--- gateways-frontend/jest.config.js
+++
@@ -1,8 +0,0 @@
-export default {
-  preset: "vite-jest",
-  testMatch: [
-    "**/tests/unit/**/*.spec.?(m)js?(x)",
-    "**/__tests__/*.?(m)js?(x)",
-  ],
-  testEnvironment: "jest-environment-jsdom",
-};

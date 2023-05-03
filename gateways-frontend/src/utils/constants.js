3d6ae930b768b1c4ff2b59af39c134519d949cbd
Jose Garcia <jogarcia@tuta.io>
2023-05-02 21:06:14 -0400

    Remove env variable

--- gateways-frontend/src/utils/constants.js
+++ gateways-frontend/src/utils/constants.js
@@ -1,8 +1,6 @@
 let hostDir = import.meta.env.VITE_HOST_DIR;
-let authDir = import.meta.env.VITE_AUTH_DIR;
 
 export const HOST_DIR = appendSlash(hostDir);
-export const AUTH_DIR = appendSlash(authDir);
 export const API_DIR = `${HOST_DIR}`;
 
 function appendSlash(url) {

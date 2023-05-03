3a82ea65241012792292abbf44133ada5a022cc7
Jose Garcia <jogarcia@tuta.io>
2023-05-03 11:00:00 -0400

    Allow all origins

--- gateways-backend/app.js
+++ gateways-backend/app.js
@@ -11,7 +11,11 @@ var indexRouter = require("./routes/index");
 
 var app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: true,
+  })
+);
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
